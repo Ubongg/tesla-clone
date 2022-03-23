ef46629826384ea49f65973d92ba70082f191481
Ubongg <ubongeffiong5@gmail.com>
2022-03-23 11:43:19 +0100

    added sections for the tesla

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 
-const Section = () => {
+const Section = ({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) => {
   return (
     <div>
-      <Wrap>
+      <Wrap bgImage={backgroundImg}>
         <ItemText>
-          <h1>Model S</h1>
-          <p>Order Online for Touchless Delivery</p>
+          <h1>{title}</h1>
+          <p>{description}</p>
         </ItemText>
         <Buttons>
           <ButtonGroup>
-            <LeftButton>Custom Order</LeftButton>
-            <RightButton>Existing Inventory</RightButton>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
           </ButtonGroup>
           <DownArrow src="/images/down-arrow.svg" />
         </Buttons>
@@ -34,6 +40,7 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 const ItemText = styled.div`
   padding-top: 15vh;
@@ -71,4 +78,6 @@ const DownArrow = styled.img`
   height: 40px;
   animation: animateDown infinite 1.5s;
 `;
-const Buttons = styled.div``;
+const Buttons = styled.div`
+  overflow: hidden;
+`;
